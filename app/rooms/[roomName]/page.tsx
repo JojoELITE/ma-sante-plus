ab4427a461fe06a78d9367c2df5ebdf3fcb49d7e
unknown <elieboulingui2@gmail.com>
2025-06-12 17:42:00 +0100

    udpate meeting

--- app/rooms/[roomName]/page.tsx
+++ app/rooms/[roomName]/page.tsx
@@ -1,27 +1,30 @@
-import { PageClientImpl } from './PageClientImpl';
-import { isVideoCodec } from '@/lib/types';
+// app/rooms/[roomName]/page.tsx
 
+import { PageClientImpl } from './PageClientImpl'
+import { isVideoCodec } from '@/lib/types'
 
-export default async function page({ params }: { params: Promise<{ roomName: string }> }) {
-  
-  // Vérifier si l'utilisateur est membre de l'organisation
-  const {roomName} = await params
-  const url = new URL(process.env.NEXT_PUBLIC_BASE_URL + `/rooms/${roomName}`);
-  const searchParams = url.searchParams;
+export default async function Page({
+  params,
+  searchParams,
+}: {
+  params: { roomName: string }
+  searchParams: { [key: string]: string | string[] | undefined }
+}) {
+  const { roomName } = params
 
-  const codecParam = searchParams.get('codec');
-  const hqParam = searchParams.get('hq');
-  const regionParam = searchParams.get('region');
+  const codecParam = searchParams.codec
+  const hqParam = searchParams.hq
+  const regionParam = searchParams.region
 
-  const codec = codecParam && isVideoCodec(codecParam) ? codecParam : 'vp9';
-  const hq = hqParam === 'true';
+  const codec = codecParam && isVideoCodec(codecParam as string) ? codecParam : 'vp9'
+  const hq = hqParam === 'true'
 
   return (
     <PageClientImpl
       roomName={roomName}
-      region={regionParam || undefined}
+      region={regionParam as string | undefined}
       hq={hq}
-      codec={codec}
+      codec={"vp9"}
     />
-  );
+  )
 }
